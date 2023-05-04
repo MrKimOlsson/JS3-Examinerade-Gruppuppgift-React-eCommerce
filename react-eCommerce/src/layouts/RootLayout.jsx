140b9b29f61b0b82a3d1e1d35bc1d2412d7221ab
Kim Olsson <mr.kim.olsson@gmail.com>
2023-05-04 22:27:15 +0200

    grid component

--- react-eCommerce/src/layouts/RootLayout.jsx
+++ react-eCommerce/src/layouts/RootLayout.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/navbar/Navbar'
 import Hero from '../components/hero/Hero'
+import GridMenu from '../components/grid/gridMenu/GridMenu'
+import Grid from '../components/grid/Grid'
+import GridButton from '../components/grid/gridButton/GridButton'
 
 const RootLayout = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <GridMenu />
+      <Grid />
+      <GridButton />
       <div className="container">
         <Outlet />
       </div>
