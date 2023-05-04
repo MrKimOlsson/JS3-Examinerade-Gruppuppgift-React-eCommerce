27d1a50e3992279ca47844b2b1e4262223097ad8
Kim Olsson <mr.kim.olsson@gmail.com>
2023-05-04 20:51:22 +0200

    hero update

--- react-eCommerce/src/components/hero/Hero.jsx
+++ react-eCommerce/src/components/hero/Hero.jsx
@@ -5,17 +5,18 @@ import HeroProduct from '../../images/625x647.svg'
 const Hero = () => {
   return (
     <div className='hero'>
-      <div className='hero-text'>
-        <h1>Welcome to bmarketo shop</h1>
-        <h2>Exclusive Chair<br />
-        gold Collection</h2>
-        <button>SHOP NOW</button>
-      </div>
-      
-      <div className='hero-img'>
-      <img src={HeroProduct} alt="" />
-      </div>
-    
+      <div className='hero-container'>
+        <div className='hero-text'>
+          <h1>Welcome to bmarketo shop</h1>
+          <h2>Exclusive Chair<br />
+          gold Collection</h2>
+          <button>SHOP NOW</button>
+        </div>
+        
+        <div className='hero-img'>
+        <img src={HeroProduct} alt="" />
+        </div>
+      </div>    
     </div>
   )
 }
