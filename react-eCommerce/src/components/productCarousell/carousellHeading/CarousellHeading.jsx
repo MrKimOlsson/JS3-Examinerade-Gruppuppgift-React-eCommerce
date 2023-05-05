7ba6c2cd6a31fa058225650fecad00271e775d2b
Kim Olsson <mr.kim.olsson@gmail.com>
2023-05-05 12:34:34 +0200

    Admin post component pending

---
+++ react-eCommerce/src/components/productCarousell/carousellHeading/CarousellHeading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './carousellHeading.css'
+
+const CarouselHeading = () => {
+  return (
+    <div className='carousellHeading'>
+        <h4>Top selling products in this week</h4>
+    </div>
+  )
+}
+
+export default CarouselHeading
--- react-eCommerce/src/pages/Home.jsx
+++ react-eCommerce/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-// import Navbar from '../components/navbar/Navbar'
 import Hero from '../components/hero/Hero'
 import GridMenu from '../components/grid/gridMenu/GridMenu'
 import Grid from '../components/grid/Grid'
 import GridButton from '../components/grid/gridButton/GridButton'
 import SpecialOffers from '../components/specialOffers/SpecialOffers'
+import ProductCarousell from '../components/productCarousell/ProductCarousell'
+import AdminPost from '../components/adminPost/AdminPost'
 
 const Home = () => {
   return (
@@ -14,6 +15,8 @@ const Home = () => {
       <Grid />
       <GridButton />
       <SpecialOffers />
+      <ProductCarousell />
+      <AdminPost/>
     </>
   )
 }
