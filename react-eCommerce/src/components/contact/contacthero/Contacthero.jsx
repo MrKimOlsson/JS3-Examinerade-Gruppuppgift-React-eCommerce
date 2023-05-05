a0540b182bdce419a7b923c9f5912bf0554b4856
G3orgeS <112631563+G3orgeS@users.noreply.github.com>
2023-05-05 19:27:30 +0200

    interactiv karta + contacthero

---
+++ react-eCommerce/src/components/contact/contacthero/Contacthero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../contacthero/contacthero.css'
+import ProductImage from '../../../images/1920x300.svg'
+
+const Contacthero = () => {
+  return (
+    <div>
+        <img src={ProductImage} alt="" id='contactheroid' />
+    </div>
+  )
+}
+
+export default Contacthero
