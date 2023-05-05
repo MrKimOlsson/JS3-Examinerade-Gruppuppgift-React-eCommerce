21deac50434b03d5857173faa68be20d44c19fda
Kim Olsson <mr.kim.olsson@gmail.com>
2023-05-05 20:29:37 +0200

    Product page

---
+++ react-eCommerce/src/components/products/productsHero/ProductsHero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductsHero = () => {
+  return (
+    <div>ProductsHero</div>
+  )
+}
+
+export default ProductsHero
--- react-eCommerce/src/pages/Shop.jsx
+++ react-eCommerce/src/pages/Shop.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Grid from '../components/grid/Grid'
 import Footer from '../components/footer/Footer'
+import GridMenu from '../components/grid/gridMenu/GridMenu'
+import ProductsHero from '../components/products/productsHero/ProductsHero'
+
 
 function Shop() {
   return (
     <>
-
+    <ProductsHero />
+    <GridMenu />
+    <Grid />
     </>
   )
 }
