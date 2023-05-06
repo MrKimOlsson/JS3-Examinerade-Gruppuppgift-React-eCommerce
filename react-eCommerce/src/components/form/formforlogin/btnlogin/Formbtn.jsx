cb87281ac96a956fc9f05d7b01073c33770ad21a
Shayes1995 <email@example.com>
2023-05-06 13:00:36 +0200

    login form

---
+++ react-eCommerce/src/components/form/formforlogin/btnlogin/Formbtn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Formbtn.css'
+
+const Formbtn = () => {
+  return (
+    <div>
+      <button className='btn-submit-login'>Submit</button>
+    </div>
+  )
+}
+
+export default Formbtn
