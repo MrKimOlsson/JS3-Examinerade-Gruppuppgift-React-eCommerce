421c99b61f080b47a2f3485832fe9d1d8059f67c
Felix Edström <felix.edstrom1@gmail.com>
2023-05-06 23:04:56 +0200

    cart changes

--- react-eCommerce/src/components/cart/totalSum/TotalSum.jsx
+++ react-eCommerce/src/components/cart/totalSum/TotalSum.jsx
@@ -8,16 +8,16 @@ function TotalSum() {
             <h2>Totalsumma</h2>
             <div className="ts-row">
                 <p>Deltotal</p>
-                <p>941,00 kr</p>
+                <p className='bold'>941,00 kr</p>
             </div>
             <div className="ts-row">
                 <p>Frakt</p>
-                <p>0,00 kr</p>
+                <p className='bold'>0,00 kr</p>
             </div>
             <div className="totalsum-calc">
                 <div className="ts-row">
                     <p>Totalsumma (incl. moms)</p>
-                    <p>951,00 kr</p>
+                    <p className='bold'>951,00 kr</p>
                 </div>
                 <button>Gå till kassan</button>
             </div>
