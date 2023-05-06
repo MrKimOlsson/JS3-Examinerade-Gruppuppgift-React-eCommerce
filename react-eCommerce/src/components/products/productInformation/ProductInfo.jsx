e127a194bbce1f3b9a6d72e71bf0f3c2c5d332a9
Kim Olsson <mr.kim.olsson@gmail.com>
2023-05-06 10:26:54 +0200

    Moved product grid component to components/products

---
+++ react-eCommerce/src/components/products/productInformation/ProductInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProductInfoMenu from './productInfoMenu/ProductInfoMenu'
+
+const ProductInfo = () => {
+  return (
+    
+    <div>ProductInfo
+        <ProductInfoMenu />
+
+
+    </div>
+  )
+}
+
+export default ProductInfo
--- react-eCommerce/src/pages/Shop.jsx
+++ react-eCommerce/src/pages/Shop.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Grid from '../components/universal/productGrid/Grid'
-import GridMenu from '../components/universal/productGrid/gridMenu/GridMenu'
+import Grid from '../components/products/productGrid/Grid'
+import GridMenu from '../components/products/productGrid/gridMenu/GridMenu'
 import WideHero from '../components/universal/wideHero/WideHero'
 import SpecialOffersWide from '../components/products/specialOffersWide/SpecialOffersWide'
 
