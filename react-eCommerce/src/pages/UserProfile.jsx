328d1e82045f9631b4d97520d2c57ee7af1de0f1
Felix Edström <felix.edstrom1@gmail.com>
2023-05-09 21:56:39 +0200

    user-profile

--- react-eCommerce/src/pages/UserProfile.jsx
+++ react-eCommerce/src/pages/UserProfile.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import UserProfile from '../components/userProfile/UserProfile'
 
 function userProfile() {
   return (
-    <div>userProfile</div>
+    <div className='up-wrapper'>
+      <UserProfile />
+    </div>
   )
 }
 
