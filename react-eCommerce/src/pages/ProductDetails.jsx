f4a9e41dbf0d82fe1b910302f166db333d937f24
Kim Olsson <mr.kim.olsson@gmail.com>
2023-05-09 19:31:09 +0200

    product link to product details with id

--- react-eCommerce/src/pages/ProductDetails.jsx
+++ react-eCommerce/src/pages/ProductDetails.jsx
@@ -4,11 +4,35 @@ import Details from '../components/products/productDetails/Details'
 import ProductInfo from '../components/products/productInformation/ProductInfo'
 import RelatedProducts from '../components/products/relatedProducts/RelatedProducts'
 import SmallProductCarousell from '../components/products/smallCarousell/SmallProductCarousell'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useFetch } from '../hooks/useFetch'
 
+export let product;
 
 function ProductDetails() {
+
+  
+  const [product, setProduct] = useState(null)
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getProduct = async () => {
+      const res = await fetch('http://localhost:9999/api/product/' + id)
+      const data = await res.json()
+      console.log(data)
+      setProduct(data)
+    }
+    getProduct()
+  }, [])
+
+  
+  // console.log(product.title)
+  
+  
   return (
     <div>
+      {/* <p>{product.title}</p> */}
       <WideHero />
       <Details />
       <ProductInfo />
--- react-eCommerce/src/pages/Products.jsx
+++ react-eCommerce/src/pages/Products.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { useState, useEffect, useCallback } from 'react'
-import Grid from '../components/products/productGrid/Grid'
+import LargeGrid from '../components/products/productGrid/largeGrid/LargeGrid'
 import GridMenu from '../components/products/productGrid/gridMenu/GridMenu'
 import WideHero from '../components/universal/wideHero/WideHero'
 import SpecialOffersWide from '../components/products/specialOffersWide/SpecialOffersWide'
@@ -10,7 +10,7 @@ function Products() {
     <>
     <WideHero />
     <GridMenu />
-    <Grid/>
+    <LargeGrid/>
     <SpecialOffersWide />
     </>
   )
